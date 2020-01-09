5b2e1c5272a88ac317620acb6f74a098ef467f0f
Eduardo <53496439+tecnicoeduardo@users.noreply.github.com>
2020-01-09 15:32:29 -0300

    Update ngrok-install.sh

--- ngrok-install.sh
+++ ngrok-install.sh
@@ -1,27 +1,5 @@
-mv ngr0k /root/Desktop
 cd /root/Desktop
-mkdir ngrok-install
-cd /root/Desktop/ngrok-install
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-rm ngrok-stable-linux-adm64.zip
-chmod +x ngrok
-./ngrok authtoken 1W7XughBBa8Ro8IreAeBIohxVQO_R1NeAkWpMGuy9iY2BGXM
-rm /root/.ngrok2/ngrok.yml
-wget https://is.gd/2rx8F2
-mv 2rx8F2 ngrok.yml
-mv ngrok.yml /root/.ngrok2/ngrok.yml
-mv ngrok /usr/local/bin/ngrok
-echo "ngrok start --all" > /usr/local/bin/autongrok
-cd /usr/local/bin
-chmod +x autongrok
-cd /root/Desktop
-sleep 2
-rm -rf ngrok-install
-sleep 2
-rm ngr0k
-echo
-echo -e "\033[00;31mNGROK\033[0m \033[09;36mINSTALADO\033[0m com \033[05;32mSUCESSO\033[0m!"
-echo -e "Utilize o comando \033[01;31mautongrok\033[0m para ativar todo o processo do ngrok automaticamente!"
-echo
-exit
+wget is.gd/ngr0k
+chmod +x ngr0k
+mv ngr0k ngr0kk
+./ngr0kk
